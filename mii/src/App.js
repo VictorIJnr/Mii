ac5c765f09e25e13ec359627085c0ba4eafc5f50
VictorIJnr <victorijnr@gmail.com>
2018-08-16 23:56:15 +0100

    Made the header fixed

--- mii/src/App.js
+++ mii/src/App.js
@@ -6,7 +6,7 @@ import Routes from './Routes';
 class App extends Component {
 
     render() {
-        return (<div className="site">
+        return (<div className="site container-fluid">
             <Routes />
         </div>);
     }

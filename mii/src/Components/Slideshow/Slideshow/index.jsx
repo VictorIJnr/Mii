3a4d9f38499e6648c348186076fe51284d794e6c
VictorIJnr <victorijnr@gmail.com>
2018-08-20 01:56:59 +0100

    Made some improvements to the slideshow help div
    UGH I've been working on this longer than I wanted to. BUT HERE WE ARE
    BOYS. ffs.

--- mii/src/Components/Slideshow/Slideshow/index.jsx
+++ mii/src/Components/Slideshow/Slideshow/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import Slider from "react-slick";
 import "../stylish.css";
 
@@ -31,7 +30,31 @@ class Slideshow extends Component {
         };
 
         return (<div className="slideshow">
-            <Slider autoFocus {...slicky}>
+            <div id="wrapper">
+                <a href="#help" data-toggle="collapse"><i className="far fa-question-circle"></i></a>
+                <a id="sneaky">
+                    You're not supposed to be here are you? No. No you're not. Don't worry,
+                    this tag shouldn't be here either. But I've spent too much time debugging
+                    this part of this website that I'm settling for this workaround. For now,
+                    this'll be gone soon. Mark my words. At 00:09 on the 20th of August 2018, I
+                    vowed to get rid of this cheeky little beaky a tag. 
+
+                    I fixed it. It's been 5 minutes and I fixed it using a z-index... Great...
+                    There's no real reason for this tag to be here now. Or you for that matter. 
+                    You can stop inspecting now. Go on. Shoo.
+                </a>
+                <div id="helper">
+                    <div id="help" className="collapse show">
+                        <p>
+                            You can use the arrow keys to cycle through the slides if you'd like. 
+                            Or if you're pretty fancy with a touch-screen laptop go ahead and 
+                            swipe that bad boy. <br /> Satisfying, isn't it?
+                        </p>
+                    </div>  
+                </div>
+            </div>
+
+            <Slider {...slicky}>
                 {/* {this.props.children} */}
 
                 <div ref={this.initSlide}>

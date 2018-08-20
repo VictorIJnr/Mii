53a692863d415bacb0357417bf26f49337e96982
VictorIJnr <victorijnr@gmail.com>
2018-08-20 14:16:05 +0100

    Fixed the slideshow helper div
    ABOUT DAMN TIME TOO. Look at the diff, it was just a bunch of width
    calculations getting fucked up, I guess. Plus I reworked how content is
    displayed on the page, may come at a detriment to the mobile rendeiring
    of the page, but oh well. Plus I realise that I misspelt rendering, but
    I'm not fixing it because Vim inside Hyper is broken af with arrow keys.

--- mii/src/Components/Content/index.jsx
+++ mii/src/Components/Content/index.jsx
@@ -4,8 +4,16 @@ import "./stylish.css";
 class Content extends Component {
 
     render() {
-        return (<div className="content">
-            {this.props.children}
+        return (<div className="content row">
+            <div className="col-md-2">
+            </div>
+
+            <div className="col-md-8">
+                {this.props.children}
+            </div>
+            
+            <div className="col-md-2">
+            </div>
         </div>);
     }
 

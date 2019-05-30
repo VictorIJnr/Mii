ac99381e06e853e1e96298e811b9a3536934d08b
VictorIJnr <victorijnr@gmail.com>
2019-05-30 21:42:05 +0100

    Added repo roulette.
    Well, I've completed it. All the READMEs can be pulled and rendered
    appropriately. Slightly annoyed about needing to alter some READMEs to
    comply with the library I'm using (because of a bug I found, look at
    issue #4) but, c'est la vie, non?

--- mii/src/Routes/Blog.jsx
+++ mii/src/Routes/Blog.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
-import ReactMarkdown from "react-markdown";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-boost";
-import { createHttpLink } from "apollo-link-http";
-import { setContext } from "apollo-link-context";
-import gql from "graphql-tag";
+import ReactMarkdown from "react-markdown/with-html";
 
 import axios from "axios";
 
 import Header from "../Components/Header";
 import Content from "../Components/Content";
-import Cutex from "../assets/Cutex";
 
 import { myToken } from "../config/tokens.json";
 
@@ -18,78 +12,59 @@ class Blog extends Component {
     constructor(props) {
         super(props);
 
-        let githubURL = "https://api.github.com/graphql";
-
-        let httpLink = createHttpLink({
-            uri: githubURL
-        });
-
-        const authLink = setContext((_, { headers }) => {
-            // Return the headers to the context so httpLink can read them
-            return {
-              headers: {
-                ...headers,
-                authorization: `bearer ${myToken}`,
-              }
-            }
-          });
-
-        // Making a client to query the GitHub API
-        this.clientQL = new ApolloClient({
-            uri: githubURL,
-            cache: new InMemoryCache(),
-            link: authLink.concat(httpLink)
-        });
+        this.state = {loaded: false, data: ""};
     }
 
     /**
      * Randomly selecting a README from one of my repos
      */
-    genReadMe() {
+    async genReadMe() {
+        let projRender;
         const repoRoulette = ["Angelic", "Cutex", "distbu", "onesie"];
         const myRepo = repoRoulette[Math.floor(Math.random() * repoRoulette.length)];
 
-        this.clientQL.query({
-            query: gql`{
-                user(login: VictorIJnr) {
-                    repository(name: ${myRepo}) {
-                        homepageUrl 
-                        url
-                    }
-                }
-            }`
-        })
-        .then(res => {
-            console.log(res.data.user.repository);
+        let headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": `bearer ${myToken}`
+        };
+
+        let axRequest = axios.get(`https://api.github.com/repos/VictorIJnr/${myRepo}/readme`, {headers});
+
+        await axRequest.then(res => {
+            projRender = atob(res.data.content);
         })
         .catch(err => console.log(JSON.stringify(err)));
+
+        this.setState({...this.state, loaded: true, data: projRender});
+        return projRender;
     }
 
     render() {
-        this.genReadMe();
+        if (!this.state.loaded) this.genReadMe();
+        let projRender = (this.state.loaded) 
+            ? <ReactMarkdown source={this.state.data} escapeHtml={false}/>
+            : <div></div>;
 
         return (<div className="site">
             <Header selected="blog" />
             <Content>
-                <h1>I genuinely don't need this...</h1>
+                <h1>I guess this is more like a showcase</h1>
                 <p>
-                    I've always thought that blogs are relatively pretentious. Especially in CS.
-                    Don't get me wrong, there are some fantastic Medium blogs that are incredibly
-                    useful. But, honestly, they're few and far between. Plus there's also&nbsp; 
-                    <a href="http://victorsepq.weebly.com/">my first attempt at a blog</a>&nbsp;
-                    which was for my EPQ... Don't worry though, I won't do that again to you, 
-                    or try to offload a design pattern for a stupidly obscure framework/language 
-                    on you. <sup><sup>But for now here's Cutex if you're interested.</sup></sup>
-                    <br/><br/>
-                    So instead of force feeding you a bunch of my stuff (if you're intrigued 
-                    there's always more on my GitHub like&nbsp;
-                    <a href="https://github.com/VictorIJnr/Cutex">Cutex</a> and Dias - an alias 
-                    for a secret project of mine, which I won't reveal for a while), I'll be 
-                    linking out to stuff I find to be rather interesting. 
-                    That's not much better is it?
+                    That is, until I decide that I fancy writing a couple blog posts. I'll just
+                    have a selection of READMEs from some of my projects appear here. Don't worry,
+                    they're not <i>that</i> boring. I hope. I like to make then somewhat fun :D.
+                    I've made it such that the READMEs for some private repos appear here as well,
+                    so it's like our little secret.
                 </p>
-                {/* <ReactMarkdown source={}/> */}
-                <Cutex />
+                
+                <p>
+                    Perhaps in the future I'll turn this into a proper "blogspace". Right now, 
+                    I'll restrain myself from showering any and everyone with my opinion on the
+                    latest JS framework or a new paper on Neuroevolution of all things. But while
+                    we're here, I'll just drop a link to the blog I made when I was making a small
+                    Android app called&nbsp;"<a href="http://victorsepq.weebly.com/">Bloopy</a>".
+                </p>
+                {projRender}
             </Content>
         </div>);
     }
--- mii/src/Routes/Mii.jsx
+++ mii/src/Routes/Mii.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Header from "../Components/Header";
 import Content from "../Components/Content";
-import Cutex from "../assets/Cutex";
 
 class Mii extends Component {
 

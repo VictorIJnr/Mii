de8fbbc42164f3c1afdd5ef950c18272ff4b5140
Victor Junior <victorijnr@gmail.com>
2022-11-29 21:13:38 +0000

    Initial Bloopy page
    We're not done yet though. We've just added a section for graphics, I
    still want to add a section for the final gameplay, and maybe one for
    music, but I don't know yet.

--- src/Routes/Projects/Bloopy/index.jsx
+++ src/Routes/Projects/Bloopy/index.jsx
@@ -2,32 +2,43 @@ import React from "react";
 
 import Content from "../../../Components/Content";
 import HighlightImage from "../../../Components/Highlight/HighlightImage";
-import MiniPlayer from "../../../Components/MiniPlayer";
 
 import bloopy from "../../../assets/images/Bloopy.png";
 
 import "./stylish.css";
+import "./responsive.css";
 
 /**
  * Project page for Bloopy.
  */
 function Bloopy() {
-    return <Content>
+    return <Content className="bloopy-content">
         <section id="bloopy-overview">
             <h1>Bloopy</h1>
             <p>
                 My first Android app.<br />
-                I created this when was 17 for my EPQ - a course where I was given free rein to create my own project, in a domain that I hadn't worked in before.<br /><br />
+                I created this when was 17 for my EPQ - a course where I was given free rein to create my own project, in a domain that I hadn't worked in before.
             </p>
         </section>
         <section id="bloopy-graphics">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/LLco2mpWQ-o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
-            <p>
-
-            </p>
-        </section>        
-        <HighlightImage image={bloopy} title="Bloopy" />
-        {/* <MiniPlayer /> */}
+            <div id="bloopy-mascot">
+                <HighlightImage image={bloopy} title="Bloopy" />
+                <p>
+                    I focused on making something cute, simple, yet distinctive. I was up against the likes of Crossy Road and Temple Run after all.<br />
+                    Objectively speaking, my titular character, Bloopy, is easily the cutest mascot of the three games.
+                </p>
+            </div>
+            <div id="bloopy-animations">
+                <div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/LLco2mpWQ-o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+                </div>
+                <p>
+                    The characters, animations, and music were all created from scratch. All the while, I was learning how to make a performant Android app,
+                    one which ran consistently regardless of which device it was installed onto.<br /><br />
+                    This lead to learning about interpolation to solve bugs in animations, which were initially dependent on a device's FPS.
+                </p>
+            </div>
+        </section>
     </Content>
 }
 

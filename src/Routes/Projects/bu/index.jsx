2c42e4a8c487e4f98e005fad4d75319692bf6e05
Victor Junior <victorijnr@gmail.com>
2022-12-13 23:59:32 +0000

    Initial work for bu
    is. I don't want to do that here though.

--- src/Routes/Projects/bu/index.jsx
+++ src/Routes/Projects/bu/index.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import MiniPlayer from "../../../Components/MiniPlayer";
 
 import Content from "../../../Components/Layout/Content";
+import SeparatorSection from "../../../Components/Layout/SeparatorSection";
 
 import "./stylish.css";
+import SeparatorDiv from "../../../Components/Layout/SeparatorDiv";
 
 /**
  * Project page for bu.
@@ -12,9 +14,20 @@ import "./stylish.css";
  */
 function UStylA(props) {
     return <Content>
+        <SeparatorSection id="bu-overview">
+            <h1>bu</h1>
+            <p>
+                I officially called this "User Stylometry Association". But, that's a mouthful.<br /><br />
+                So this is "bu", derived from the fact that I wanted to create a means of allowing virtual assistants to feel more
+                <b><i>b</i></b>espoke for <b><i>u</i></b>sers.
+            </p>
+        </SeparatorSection>
+        <SeparatorSection id="bu-intro">
+            <SeparatorDiv id="bu-history-video">
+                <MiniPlayer src="https://d1cuiew8u1gk5n.cloudfront.net/bu/history.mp4" />
+            </SeparatorDiv>
+        </SeparatorSection>
         <div id="bu-overview">
-            {/* <MiniPlayer src="https://d1cuiew8u1gk5n.cloudfront.net/bu/history.mp4" /> */}
-            <MiniPlayer src="https://d1cuiew8u1gk5n.cloudfront.net/bloopy/gameplay.mp4" />
         </div>
     </Content>
 }

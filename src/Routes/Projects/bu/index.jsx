8c10b841380adb7b9a09cab837ef12d9b2d7a670
Victor Junior <victorijnr@gmail.com>
2022-12-14 23:59:19 +0000

    Working on bu
    We're really just getting started. I've updated the description, and now
    I'm adding an overlay to the gource video.

--- src/Routes/Projects/bu/index.jsx
+++ src/Routes/Projects/bu/index.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import MiniPlayer from "../../../Components/MiniPlayer";
 
 import Content from "../../../Components/Layout/Content";
+import SeparatorDiv from "../../../Components/Layout/SeparatorDiv";
 import SeparatorSection from "../../../Components/Layout/SeparatorSection";
 
 import "./stylish.css";
-import SeparatorDiv from "../../../Components/Layout/SeparatorDiv";
 
 /**
  * Project page for bu.
@@ -17,13 +17,19 @@ function UStylA(props) {
         <SeparatorSection id="bu-overview">
             <h1>bu</h1>
             <p>
-                I officially called this "User Stylometry Association". But, that's a mouthful.<br /><br />
-                So this is "bu", derived from the fact that I wanted to create a means of allowing virtual assistants to feel more
-                <b><i>b</i></b>espoke for <b><i>u</i></b>sers.
+                I officially called this "User Stylometry Association", but that's a mouthful.<br />
+                So this is "bu", derived from the fact that I wanted to create a means of allowing virtual assistants to feel more 
+                <span> b</span>espoke for <span>u</span>sers.
             </p>
         </SeparatorSection>
         <SeparatorSection id="bu-intro">
             <SeparatorDiv id="bu-history-video">
+                <div id="bu-mini-player-overlay">
+                    <div id="bu-overlay-colour"/>
+                    <div id="bu-overlay-content">
+                        <h2>So, why spend 6 months working on this?</h2>
+                    </div>
+                </div>
                 <MiniPlayer src="https://d1cuiew8u1gk5n.cloudfront.net/bu/history.mp4" />
             </SeparatorDiv>
         </SeparatorSection>

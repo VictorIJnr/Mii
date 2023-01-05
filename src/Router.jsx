f7361813c119b26b49f6c0085cc87304e7cba7b2
Victor Junior <victorijnr@gmail.com>
2023-01-05 21:20:14 +0000

    Here's a broken 404 page
    is.

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,6 +9,7 @@ import Projects from "./Routes/Projects/";
 import Bloopy from "./Routes/Projects/Bloopy/";
 import UStylA from "./Routes/Projects/bu";
 import WaffleCo from "./Routes/Projects/WaffleCo";
+import NotFound from "./Routes/NotFound";
 
 /**
  * Handles routing between the different pages of the website.
@@ -22,6 +23,8 @@ function Router() {
         <Route path="/projects/bloopy" element={<Bloopy />} />
         <Route path="/projects/bu" element={<UStylA />} />
         <Route path="/projects/waffles" element={<WaffleCo />} />
+        
+        <Route path="*" element={<NotFound />} />
     </Routes>
 }
 

86379cade49b240c1c2e4cf7d3282afc722876ba
Victor Junior <victorijnr@gmail.com>
2023-01-06 23:55:10 +0000

    Here's a working 404 page
    I want to make it randomly pick a page to suggest though, instead of
    being hard-coded to my projects.

--- src/Components/Navigation/TextLink/index.jsx
+++ src/Components/Navigation/TextLink/index.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Link } from "react-router-dom";
+
 import "./stylish.css";
 
 /* 
     Consistently renders links to external websites.
 */
 function TextLink(props) {
-    return <a className="text-link" href={props.url}>{props.text}</a>
+    return <>
+        {props.path !== ""
+        ? <Link to={props.path} className="text-link">{props.text}</Link>
+        : <a className="text-link" href={props.url}>{props.text}</a>
+    }
+    </>
 }
 
 TextLink.propTypes = {
     //? The text that should be rendered for the link.
     text: PropTypes.string.isRequired,
+    
+    //? The path, within Mii, where the user should be taken.
+    path: PropTypes.string,
 
     //? The destination URL to take the user upon click.
-    url: PropTypes.string.isRequired
+    url: PropTypes.string
+}
+
+TextLink.defaultProps = {
+    path: "",
+    url: ""
 }
 
 export default TextLink;
--- src/Routes/NotFound/index.jsx
+++ src/Routes/NotFound/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function NotFound() {
+import Content from "../../Components/Layout/Content";
+import TextLink from "../../Components/Navigation/TextLink";
 
+/* Renders a basic 404 page. */
+function NotFound(props) {
+    return <Content>
+        <h1 style={{ marginTop: 0 }}>What did you do?</h1>
+        <p style={{ textAlign: "center" }}>
+            Either you tried to explore somewhere that doesn't exist, or my hosting messed up.
+            <br />
+            Regardless, how about you take a look at <TextLink path="projects" text="some projects I created" />.
+        </p>
+    </Content>
 }
 
 export default NotFound;

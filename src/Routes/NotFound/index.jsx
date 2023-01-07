3f2335610164b77ff53425387e282f1c2c7bab6f
Victor Junior <victorijnr@gmail.com>
2023-01-07 23:56:18 +0000

    Added some randomness to the 404 page
    is.

--- src/Routes/NotFound/index.jsx
+++ src/Routes/NotFound/index.jsx
@@ -5,12 +5,29 @@ import TextLink from "../../Components/Navigation/TextLink";
 
 /* Renders a basic 404 page. */
 function NotFound(props) {
+    const routes = [
+        {
+            path: "projects",
+            displayText: "some projects I created"
+        },
+        {
+            path: "career",
+            displayText: "my professional career so far"
+        },
+        {
+            path: "playground",
+            displayText: "some nonsense I've put together"
+        }
+    ];
+
+    const routeInfo = routes[Math.floor(Math.random() * routes.length)];
+
     return <Content>
         <h1 style={{ marginTop: 0 }}>What did you do?</h1>
         <p style={{ textAlign: "center" }}>
             Either you tried to explore somewhere that doesn't exist, or my hosting messed up.
             <br />
-            Regardless, how about you take a look at <TextLink path="projects" text="some projects I created" />.
+            Regardless, how about you take a look at <TextLink path={routeInfo.path} text={routeInfo.displayText} />.
         </p>
     </Content>
 }
